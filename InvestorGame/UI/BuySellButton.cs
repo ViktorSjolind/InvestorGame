3e1c6cc49e7f9884328bc3ac8378b2d1c000a184
ViktorSjolind <viktor.sjolind@gmail.com>
2018-08-19 15:04:38 +0300

    Added navbar

---
+++ InvestorGame/UI/BuySellButton.cs
@@ -0,0 +1,50 @@
+﻿using InvestorGame.Models;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvestorGame.UI
+{
+    internal class BuySellButton
+    {
+        private Texture2D Texture;
+        private Vector2 Position;
+        public BuySellButtonState State;
+
+        public void Initialize(GraphicsDevice graphicsDevice)
+        {
+            Texture = new Texture2D(graphicsDevice, 1, 1);
+            Texture.SetData(new Color[] { OwnColors.Black });
+            Position = new Vector2(0, 620);
+            State = BuySellButtonState.None;
+        }
+
+
+        public void Update()
+        {
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch, SpriteFont bigFont)
+        {
+            //Font size is compiled as 36px, scaled down for better quality
+            if(State == BuySellButtonState.None)
+            {
+
+            }
+            if (State == BuySellButtonState.Buy)
+            {
+                spriteBatch.DrawString(bigFont, "Buy:", new Vector2(100, 650), OwnColors.White, 0, new Vector2(0, 0), 0.5f, SpriteEffects.None, 0);
+            }
+            if (State == BuySellButtonState.Sell)
+            {
+                spriteBatch.DrawString(bigFont, "Sell:", new Vector2(100, 650), OwnColors.White, 0, new Vector2(0, 0), 0.5f, SpriteEffects.None, 0);
+            }
+            
+        }
+    }
+}
---
+++ InvestorGame/UI/Navbar.cs
@@ -0,0 +1,59 @@
+﻿using InvestorGame.Models;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvestorGame.UI
+{
+    internal class Navbar
+    {
+        private Texture2D Texture;
+        private Vector2 Position;
+        private BuySellButton buySellButton;
+        private Lot SelectedLot;
+
+        public void Initialize(GraphicsDevice graphicsDevice)
+        {
+            Texture = new Texture2D(graphicsDevice, 1, 1);
+            Texture.SetData(new Color[] { OwnColors.Black });
+            Position = new Vector2(0, 620);
+            buySellButton = new BuySellButton();
+            buySellButton.Initialize(graphicsDevice);
+            SelectedLot = null;
+        }
+
+        public void UpdateSelected(Lot lot)
+        {
+            SelectedLot = lot;
+        }
+
+        public Lot GetSelected()
+        {
+            return SelectedLot;
+        }
+
+
+        public void Update()
+        {
+
+        }
+        
+        public void Draw(SpriteBatch spriteBatch, SpriteFont bigFont)
+        {
+            spriteBatch.Draw(Texture, new Rectangle((int)Position.X, (int)Position.Y, 1280, 100), Color.White);
+            buySellButton.Draw(spriteBatch, bigFont);
+            
+
+            if (SelectedLot != null)
+            {
+                spriteBatch.DrawString(bigFont, "Value:", new Vector2(Position.X + 200, Position.Y + 30), OwnColors.White, 0, new Vector2(0, 0), 0.5f, SpriteEffects.None, 0);
+                spriteBatch.DrawString(bigFont, SelectedLot.Value.ToString(), new Vector2(Position.X + 280, Position.Y + 30), OwnColors.White, 0, new Vector2(0, 0), 0.5f, SpriteEffects.None, 0);
+            }
+            
+        }
+    }
+}
